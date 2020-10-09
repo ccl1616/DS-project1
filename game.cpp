f8b0b31a1b5b32e3162d6321784e2752b6a88fd4
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2020-10-09 21:17:11 +0800

    change some of tetris's ref point, to make it drop normal

--- game.cpp
+++ game.cpp
@@ -24,11 +24,30 @@ struct Point{
 };
 std::array<std::array<bool, MAX_col>, MAX_row> game;
 
-const std::array<std::array<Point, 4>, 4> spots{{
-            {Point(-1,-0),Point(-1,1),Point(-1,2),Point(0,1)},
-            {Point(-2,1),Point(-1,0),Point(-1,1),Point(0,1)},
+const std::array<std::array<Point, 4>, 19> spots{{
+            {Point(-1,-1),Point(-1,0),Point(-1,1),Point(0,0)}, // special
+            {Point(-2,0),Point(-1,-1),Point(-1,0),Point(0,0)}, // spacial
             {Point(-1,1),Point(0,0),Point(0,1),Point(0,2)},
-            {Point(-2,0),Point(-1,0),Point(-1,1),Point(0,0)}
+            {Point(-2,0),Point(-1,0),Point(-1,1),Point(0,0)},
+
+            {Point(-2,0),Point(-1,0),Point(0,0),Point(0,1)},
+            {Point(-1,0),Point(-1,1),Point(-1,2),Point(0,0)},
+            {Point(-2,-1),Point(-2,0),Point(-1,0),Point(0,0)}, //special
+            {Point(-1,2),Point(0,0),Point(0,1),Point(0,2)},
+
+            {Point(-2,1),Point(-1,1),Point(0,0),Point(0,1)},
+            {Point(-1,0),Point(0,0),Point(0,1),Point(0,2)},
+            {Point(-2,0),Point(-2,1),Point(-1,0),Point(0,0)},
+            {Point(-1,-2),Point(-1,-1),Point(-1,0),Point(0,0)}, //special
+
+            {Point(-1,1),Point(-1,2),Point(0,0),Point(0,1)},
+            {Point(-2,-1),Point(-1,-1),Point(-1,0),Point(0,0)}, //spacial
+            {Point(-1,-1),Point(-1,0),Point(0,0),Point(0,1)}, //special
+            {Point(-2,1),Point(-1,0),Point(-1,1),Point(0,0)},
+
+            {Point(-3,0),Point(-2,0),Point(-1,0),Point(0,0)},
+            {Point(0,0),Point(0,1),Point(0,2),Point(0,3)},
+            {Point(-1,0),Point(-1,1),Point(0,0),Point(0,1)}
 }};
 
 class Tetris{
@@ -43,15 +62,43 @@ class Tetris{
         Tetris(string name,int p, int m) :name(name),pos(p),move(m) {
             char a = name[0];
             int num = name[1]-'0';
-            if(a=='T'){
+            if(a == 'T'){
                 if(num==1) {id=0; height=2; width=3;}
                 else if(num==2) {id=1; height=3; width=2;}
                 else if(num==3) {id=2; height=2; width=3;}
                 else {id=3; height=3; width=2;}
             }
+            else if(a == 'L'){
+                if(num==1) {id=4; height=3; width=2;}
+                else if(num==2) {id=5; height=2; width=3;}
+                else if(num==3) {id=6; height=3; width=2;}
+                else {id=7; height=2; width=3;}
+            }
+            else if(a == 'J'){
+                if(num==1) {id=8; height=3; width=2;}
+                else if(num==2) {id=9; height=2; width=3;}
+                else if(num==3) {id=10; height=3; width=2;}
+                else {id=11; height=2; width=3;}
+            }
+            else if(a == 'S'){
+                if(num==1) {id=12; height=2; width=2;}
+                else {id=13; height=2; width=2;}
+            }
+            else if(a == 'Z'){
+                if(num==1) {id=14; height=2; width=2;}
+                else {id=15; height=2; width=2;}
+            }
+            else if(a == 'I'){
+                if(num==1) {id=16; height=4; width=1;}
+                else {id=17; height=1; width=4;}
+            }
+            else if(a == 'O'){
+                id=18; height=2; width=2;
+            }
+
         }
         bool valid_in_board(){
-            if(pos + width + move > board_col) {
+            if(pos + width + move > board_col || pos+move < 0) {
                 cout << "over width\n";
                 return false;
             }
@@ -79,21 +126,41 @@ class GameBoard{
         ~GameBoard(){ }
         bool drop_move(Tetris falling){
             // check valid to drop or not
-            int x = board_row-1-height_log[falling.pos];
+            int highest = height_log[falling.pos];
             int y = falling.pos;
+            if(falling.id == 11){
+                y = falling.pos+2;
+                highest = height_log[y];
+            }
+            else if(falling.id == 0 || falling.id == 1 || falling.id == 6 || falling.id == 13 || falling.id == 14){
+                y = falling.pos+1;
+                highest = height_log[y];
+            }
+            else{
+                for(int i = 1; i<=(falling.width-1) && i<= board_col; i ++){
+                    if( height_log[falling.pos+i] > highest )
+                        highest = height_log[i];
+                }
+                y = falling.pos;
+            }
+            int x = board_row-1-highest; 
+            cout << x << "," << y << endl;
             
+            // 要先判完 確定可以放 才可以四個都放進去
             for(int i = 0; i < 4; i ++){
                 Point dir = spots[falling.id][i];
                 //cout << "x:" <<x << "," << "y:" << y << endl;
                 //cout << dir.x << "," << dir.y << endl;
-                //cout << "result" << x+dir.x << "," << y+dir.y+falling.move << endl;
+                cout << "going to put:" << x+dir.x << "," << y+dir.y+falling.move << endl;
                 if(game[x+dir.x][y+dir.y+falling.move]) return false; // occupied
-                else {
-                    game[x+dir.x][y+dir.y+falling.move] = 1;
-                    // update log
-                    rowfull_log[x+dir.x]++;
-                    height_log[y+dir.y+falling.move]++;
-                }
+            }
+            for(int i = 0; i < 4; i ++){
+                Point dir = spots[falling.id][i];
+                game[x+dir.x][y+dir.y+falling.move] = 1;
+                // update log
+                rowfull_log[x+dir.x]++;
+                //height_log[y+dir.y+falling.move]++; //這裡出錯了!!
+                height_log[y+dir.y+falling.move] = max(height_log[y+dir.y+falling.move]++,board_row-(x+dir.x));
             }
             return true;
         } 
@@ -124,7 +191,7 @@ int main(int argc, char** argv)
     string inputName;
     string end = "End";
     int pos = 0, move = 0;
-    while( cin >> inputName){
+    while( cin >> inputName ){
         if( inputName.compare(end) == 0)
             break;
         else{
@@ -132,7 +199,7 @@ int main(int argc, char** argv)
             Tetris now(inputName,pos-1,move);
             // check if out boundary
             if(! now.valid_in_board() ) {
-                cout << "not in board\n";
+                cout << "input is out of boundary\n";
                 return 0;
             }
             // drop_move
