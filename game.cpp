9f3d88ce80935ee42b3f7ab76800f2dee1cb7fc4
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2020-10-10 12:02:38 +0800

    drop function complete

--- game.cpp
+++ game.cpp
@@ -271,7 +271,7 @@ int main(int argc, char** argv)
         }
         cout << endl;
     }
-    //cout << "1 2 3 4 5 6 7 8 9 0 1 2\n";
+    cout << "1 2 3 4 5 6 7 8 9 0 1 2\n";
     cout << "end of game, remember to remove this line!!\n"; 
 	return 0;
 }
